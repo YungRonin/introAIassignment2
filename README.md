54ebca96551f6ecff3c98060f2353284234c7560
YungRonin <dylanjforster8@gmail.com>
2017-05-01 12:12:32 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# introAIassignment2" 
---
+++ src/frogClass.java
@@ -0,0 +1,13 @@
+
+public class frogClass {
+	
+	private int frogNumber;
+	private String frogWord;
+	
+	public frogClass(int intArg, String stringArg){
+		
+		this.frogNumber = intArg;
+		this.frogWord = stringArg;
+	}
+
+}
