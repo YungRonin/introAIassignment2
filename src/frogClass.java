230b712551be0bde432e7b0267517dfcdf672e1e
YungRonin <dylanjforster8@gmail.com>
2017-05-01 13:49:00 +1000

    readFile method implmented

--- src/frogClass.java
+++ src/frogClass.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class frogClass {
 	
@@ -8,6 +12,37 @@ public class frogClass {
 		
 		this.frogNumber = intArg;
 		this.frogWord = stringArg;
+	}
+	
+	public void readFile(String fileName) throws IOException{
+		
+		try{
+			FileReader reader = new FileReader(fileName);
+			BufferedReader input = new BufferedReader(reader);
+			
+			String command = input.readLine();
+			
+			if(command.equals("TELL")){
+				aquireKnowledge(input.readLine());
+			}
+			else if(command.equals("ASK")){
+				formulateQuerey(input.readLine());
+			}
+			
+		}
+		catch(FileNotFoundException ex){
+			System.out.println("not found " + ex);
+		}
+	}
+
+	private void formulateQuerey(String readLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aquireKnowledge(String readLine) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
